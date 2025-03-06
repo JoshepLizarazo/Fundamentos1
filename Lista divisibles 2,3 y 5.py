d3136ff2ff6da772b58a28a53eb2a0d282985f0d
Joshep Jared Lizarazo Monterto <joshep2250150@correo.uis.edu.co>
2025-03-06 12:37:31 -0500

    Taller de Clase 6/03/2025
    1. Convierta un número dado por el usuario (N) en su representación en base b (2<=b<10).
    2. Escriba una lista de números de 2 hasta n, cuando sean divisibles por 2, 3 o 5.
    3. Separe las palabras de una frase dada por el usuario, por los espacios.
    4. Presente un menú con las operaciones aritméticas (+, -, *, /) y haga la operación seleccionada. Debe repetir el proceso hasta que se escoja la opción salir.
    5. Convierta una palabra a PigLatin

---
+++ Lista divisibles 2,3 y 5.py
@@ -0,0 +1,10 @@
+n = int(input("Cantidad de números: "))
+
+for i in range(2, n+1):
+    if i % 2 == 0: 
+        print (i)
+    elif i % 3 == 0:
+        print (i)
+    elif i % 5 == 0:
+        print (i)
+    
