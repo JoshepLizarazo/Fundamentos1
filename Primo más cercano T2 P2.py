16167da96b3d8be5b71454bdf1ddb6346a0d5bba
Joshep Jared Lizarazo Monterto <joshep2250150@correo.uis.edu.co>
2025-03-11 11:58:15 -0500

    Puntos 2 y 3 del taller 2

---
+++ Primo más cercano T2 P2.py
@@ -0,0 +1,28 @@
+
+
+n = int(input("Introduzca un número mayor que 2: "))
+if n <= 2:
+    print ("Error: Introduzca un número mayor que 2")
+d = 2
+i = n
+np = False 
+fin = False 
+while fin == False:
+    if fin == False:
+        np = False
+        d = 2
+        while np == False:
+                if i % d == 0:
+                    np = True
+                    i = i-1
+                elif d == i-1:
+                    np = True
+                    fin = True     
+                else:
+                    d = d + 1
+    
+print("El primo más cercano es: " + str(i))           
+
+
+
+
