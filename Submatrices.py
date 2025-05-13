5746c3c8c828f11b6c5cc64511214417466b1dff
Joshep Jared Lizarazo Monterto <joshep2250150@correo.uis.edu.co>
2025-05-13 11:59:18 -0500

    Trabajo de práctica para Parcial
    INCOMPLETO: Hace falta culminar el trabajo y comentar

---
+++ Submatrices.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+def submatrices(A, n0, n1):
+    sub = []
+    for i in range(sub):
+        c = []
+        for j in range(sub):
+            c.append(A[j,i])
+        sub.append(c)
+    return np.array(sub)
+
+matriz = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+]
+
+
+
